e7b0f5c4f3268883bdf811f63ffb85ff32b78a8b
Carlos Henrique Magalhães <carlosdeveloper669@gmail.com>
2023-04-12 12:35:34 -0300

    type_event (primeiro envio) - versão 1.0

---
+++ users/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+# Create your views here.
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        user_name = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+        
+        if not password == confirm_password:
+            return redirect('/users/register')
+
+        return HttpResponse("teste")
+
+    
