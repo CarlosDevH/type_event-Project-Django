088d8ab444d8f458ee3b4ef04f73c1dd98612266
Carlos Henrique Magalhães <carlosdeveloper669@gmail.com>
2023-04-14 11:56:15 -0300

    type_event ( sexta atualização) - versão 1.6
    Versão final do primeiro conjunto de atualizações, da beta da aplicação, a partir das próximas atualizações as versões seguirão como, 2.0
    
    da versão 1.0 até a 1.6, foram feitas diversas modificações no projeto, visando a criação do login e cadastro do usuário, além do cadastro de eventos (funcionalidade da aplicação).
    
    Mensagens de sucesso, erro ou exceções forma adicionadas para facilitar a compreensão do usuário.
    
    Por fim, a modelagem do banco de dados fora feita, objetivando  o armazenamento dos dados do usuário.

--- eventos/models.py
+++ eventos/models.py
@@ -1,9 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Event(models.Model):
+    creator = models.ForeignKey(User, on_delete = models.DO_NOTHING, null = True, blank = True)
     name = models.CharField(max_length=200)
     description = models.TextField()
     start_date = models.DateField()
+    end_date = models.DateField()
     workload = models.IntegerField()
-    logo = models.ImageField
+    logo = models.ImageField(upload_to = 'logo')
+
+    #paletas de cores
+    cor_principal = models.CharField(max_length = 7)
+    cor_secundaria = models.CharField(max_length = 7)
+    cor_fundo = models.CharField(max_length = 7)
+
+    def __str__(self) -> str:
+        return self.name
--- eventos/views.py
+++ eventos/views.py
@@ -1,8 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Event
+from django.urls import reverse
+from django.contrib import messages
+from django.contrib.messages import constants
 
 # Create your views here.
 @login_required
 def new_event(request):
     if request.method == "GET":
         return render(request, 'new_event.html')
+    elif request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        start_date = request.POST.get('start_data')
+        end_date = request.POST.get('end_data')
+        workload = request.POST.get('workload')
+
+        cor_principal = request.POST.get('cor_principal')
+        cor_secundaria = request.POST.get('cor_secundaria')
+        cor_fundo = request.POST.get('cor_fundo')
+        
+        logo = request.FILES.get('logo')
+
+        event = Event(
+            creator = request.user,
+            name = name,
+            description = description,
+            start_date = start_date,
+            end_date = end_date,
+            workload = workload,
+            cor_principal = cor_principal,
+            cor_secundaria = cor_secundaria,
+            cor_fundo = cor_fundo,
+            logo = logo
+        )
+
+        event.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Evento cadastrado com sucesso')
+        return redirect(reverse('new_event'))
